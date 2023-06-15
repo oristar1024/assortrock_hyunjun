0d0a1536a5742501d7a5318d599ff71c2ddc1573
oristar1024 <tronia94@gmail.com>
2023-06-15 11:02:17 +0900

    Unreal 프로젝트 추가

---
+++ UE_Practice/Source/UE_Practice/UE_Practice.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_Practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Practice, "UE_Practice" );
