736723886c07db2d514ea39e9186a11b768d65fe
oristar1024 <tronia94@gmail.com>
2023-06-15 13:03:43 +0900

    언리얼 기본 업데이트

---
+++ UE_Practice/Source/UE_Practice/GameMode/BaseLevelMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseLevelMode.h"
+
+ABaseLevelMode::ABaseLevelMode()
+{
+
+}
+
+void ABaseLevelMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
